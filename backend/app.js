e1bf926eb583809fa11320f6c871536ce7754c01
imadkaanane <imadkaanane@gmail.com>
2022-01-07 18:39:29 +0100

    modif app.js

--- backend/app.js
+++ backend/app.js
@@ -18,14 +18,6 @@ secretCookie = process.env.SECRET_COOKIE;
 mongoose.connect(`${mongoConnect}`,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(`Connexion à MongoDB réussie avec le profil : ${profilName}!`))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
  
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }),
         helmet.crossOriginOpenerPolicy({ policy: "same-origin" }),
@@ -52,6 +44,12 @@ app.use(cookieSession({
   domain: host,
 }))
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 app.use(bodyParser.json());
